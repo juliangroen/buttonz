b8139c543012e9a38902e1ff59c97d621eb41895
Julian Groen <julian.groen@wgu.edu>
2024-08-01 01:30:32 -0600

    improved arrows; added to display

--- game.py
+++ game.py
@@ -13,7 +13,10 @@ class App:
             "button_b": [(0, 0, 16, 16, 16, 13), (0, 16, 16, 16, 16, 13)],
             "button_x": [(0, 0, 32, 16, 16, 13), (0, 16, 32, 16, 16, 13)],
             "button_y": [(0, 0, 48, 16, 16, 13), (0, 16, 48, 16, 16, 13)],
-            "button_right": [(0, 0, 64, 16, 16, 13), (0, 16, 64, 16, 16, 13)],
+            "button_down": [(0, 0, 64, 16, 16, 13), (0, 16, 64, 16, 16, 13)],
+            "button_right": [(0, 0, 80, 16, 16, 13), (0, 16, 80, 16, 16, 13)],
+            "button_left": [(0, 0, 96, 16, 16, 13), (0, 16, 96, 16, 16, 13)],
+            "button_up": [(0, 0, 112, 16, 16, 13), (0, 16, 112, 16, 16, 13)],
         }
         pyxel.run(self.update, self.draw)
 
@@ -23,12 +26,15 @@ class App:
     def draw(self):
         pyxel.cls(0)
         pyxel.rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 13)
-        self.animated_button(24, 8, self.sprites["button_y"])
-        self.animated_button(8, 24, self.sprites["button_x"])
-        self.animated_button(40, 24, self.sprites["button_b"])
-        self.animated_button(24, 40, self.sprites["button_a"])
+        self.animated_button(80, 8, self.sprites["button_y"])
+        self.animated_button(64, 24, self.sprites["button_x"])
+        self.animated_button(96, 24, self.sprites["button_b"])
+        self.animated_button(80, 40, self.sprites["button_a"])
 
-        self.animated_button(64, 64, self.sprites["button_right"])
+        self.animated_button(24, 8, self.sprites["button_up"])
+        self.animated_button(8, 24, self.sprites["button_left"])
+        self.animated_button(40, 24, self.sprites["button_right"])
+        self.animated_button(24, 40, self.sprites["button_down"])
 
     def animated_button(self, x, y, sprite):
         if pyxel.frame_count % 60 < 15:
