d005a71f475532d5caa5d5b305c965ceac92df09
Julian Groen <julian.groen@wgu.edu>
2024-08-01 11:08:13 -0600

    got the buttons scrolling left to right

--- game.py
+++ game.py
@@ -1,13 +1,18 @@
+import random
+import sys
+
 import pyxel
 
 WINDOW_WIDTH = 256
 WINDOW_HEIGHT = 256
+FPS = 60
 
 
 class App:
     def __init__(self):
-        pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT, fps=60)
+        pyxel.init(WINDOW_WIDTH, WINDOW_HEIGHT, fps=FPS)
         pyxel.load("./assets.pyxres")
+        self.running = True
         self.sprites = {
             "button_a": [(0, 0, 0, 16, 16, 13), (0, 16, 0, 16, 16, 13)],
             "button_b": [(0, 0, 16, 16, 16, 13), (0, 16, 16, 16, 16, 13)],
@@ -18,10 +23,17 @@ class App:
             "button_left": [(0, 0, 96, 16, 16, 13), (0, 16, 96, 16, 16, 13)],
             "button_up": [(0, 0, 112, 16, 16, 13), (0, 16, 112, 16, 16, 13)],
         }
+        self.sp_mid_x = (WINDOW_WIDTH // 2) - 8
+        self.sp_mid_y = (WINDOW_HEIGHT // 2) - 8
+        self.bpm = 120
+        self.fpb = (FPS * 60) // self.bpm
+        self.current_buttons = []
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        pass
+        if pyxel.frame_count % self.fpb == 0:
+            self.button_streamer()
+            self.button_scroller()
 
     def draw(self):
         pyxel.cls(0)
@@ -36,11 +48,29 @@ class App:
         self.animated_button(40, 24, self.sprites["button_right"])
         self.animated_button(24, 40, self.sprites["button_down"])
 
+        for button in self.current_buttons:
+            self.animated_button(button["offset"], self.sp_mid_y, button["sprite"])
+
     def animated_button(self, x, y, sprite):
-        if pyxel.frame_count % 60 < 15:
+        if pyxel.frame_count % self.fpb < (self.fpb // 2):
             pyxel.blt(x, y, *sprite[1])
         else:
             pyxel.blt(x, y, *sprite[0])
+
+    def button_generator(self):
+        while self.running:
+            yield random.choice(list(self.sprites.values()))
+
+    def button_streamer(self):
+        sprite = next(self.button_generator())
+        counter = 1
+        offset = -sprite[0][3] * counter
+        self.current_buttons.append({"offset": offset, "sprite": sprite})
+        counter += 1
+
+    def button_scroller(self):
+        for button in self.current_buttons:
+            button["offset"] += (button["sprite"][0][3]) + 8
 
 
 App()
