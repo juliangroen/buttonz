3a570cf5d64a92faad451760adcd1cf3e1114388
Julian Groen <julian.groen@wgu.edu>
2024-08-01 00:28:58 -0600

    added arrow art

--- game.py
+++ game.py
@@ -13,6 +13,7 @@ class App:
             "button_b": [(0, 0, 16, 16, 16, 13), (0, 16, 16, 16, 16, 13)],
             "button_x": [(0, 0, 32, 16, 16, 13), (0, 16, 32, 16, 16, 13)],
             "button_y": [(0, 0, 48, 16, 16, 13), (0, 16, 48, 16, 16, 13)],
+            "button_right": [(0, 0, 64, 16, 16, 13), (0, 16, 64, 16, 16, 13)],
         }
         pyxel.run(self.update, self.draw)
 
@@ -26,6 +27,8 @@ class App:
         self.animated_button(8, 24, self.sprites["button_x"])
         self.animated_button(40, 24, self.sprites["button_b"])
         self.animated_button(24, 40, self.sprites["button_a"])
+
+        self.animated_button(64, 64, self.sprites["button_right"])
 
     def animated_button(self, x, y, sprite):
         if pyxel.frame_count % 60 < 15:
